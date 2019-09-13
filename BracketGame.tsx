12216220556735d76165bee92ff56295c18cd6e4
Charlie Zhang <charlie.zhang@robotemi.com>
2019-09-13 15:29:58 +0800

    Add ES5 support

--- BracketGame.tsx
+++ BracketGame.tsx
@@ -26,6 +26,56 @@ export function numberToString(x: number) {
 	return x.toLocaleString("en-US").replace(/,/g, thousandSep)
 }
 
+interface ObjectConstructor {
+	assign<T, U>(target: T, source: U): T & U
+	assign<T, U, V>(target: T, source1: U, source2: V): T & U & V
+	assign<T, U, V, W>(
+		target: T,
+		source1: U,
+		source2: V,
+		source3: W,
+	): T & U & V & W
+	assign(target: any, ...sources: any[]): any
+}
+
+if (typeof Object.assign !== "function") {
+	// Must be writable: true, enumerable: false, configurable: true
+	Object.defineProperty(Object, "assign", {
+		value: function assign(target, varArgs) {
+			// .length of function is 2
+			"use strict"
+			if (target === null || target === undefined) {
+				throw new TypeError(
+					"Cannot convert undefined or null to object",
+				)
+			}
+
+			var to = Object(target)
+
+			for (var index = 1; index < arguments.length; index++) {
+				var nextSource = arguments[index]
+
+				if (nextSource !== null && nextSource !== undefined) {
+					for (var nextKey in nextSource) {
+						// Avoid bugs when hasOwnProperty is shadowed
+						if (
+							Object.prototype.hasOwnProperty.call(
+								nextSource,
+								nextKey,
+							)
+						) {
+							to[nextKey] = nextSource[nextKey]
+						}
+					}
+				}
+			}
+			return to
+		},
+		writable: true,
+		configurable: true,
+	})
+}
+
 Object.assign(window, { twemoji })
 interface BracketGameProps {
 	game: Game
